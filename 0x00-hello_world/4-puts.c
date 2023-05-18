93a46526ba21e2e176c67c5d2ee5ae75e06cd41d
felister231 <felimu2030@gmail.com>
2023-05-18 08:09:55 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+*file: puts.c
+*discription: This file prints  a string using puts
+*author: Felister
+*
+*/
+
+#include<stdio.h>
+/*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
